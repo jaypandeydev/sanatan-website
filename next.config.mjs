7c80681c36a1b256e6b0c16f3032ef82b3b50707
Jay prakash Pandey <jayprakashpandey@Jays-Mac-mini.local>
2025-08-03 13:39:50 +0530

    changed two files called deploy.yml and nextconfig file to fix reset issue

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  generateBuildId: async () => {
+    return `${Date.now()}`; // force unique build ID to avoid chunk mismatch
+  },
 }
 
 export default nextConfig
